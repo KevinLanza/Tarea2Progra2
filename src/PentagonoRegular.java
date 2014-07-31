7b999d1f174a221cbbf7dc8d6759c4404702a1ba
KevinLanza <klanza03@gmail.com>
2014-07-31 07:23:11 -0600

    Update PentagonoRegular.java

--- src/PentagonoRegular.java
+++ src/PentagonoRegular.java
@@ -1,26 +1,26 @@
 
 public class PentagonoRegular {
 	
-	int Lado;
+	int Lado;//Se crea la variable lado para el desarrollo del ejercicio
 	
-	PentagonoRegular (int Lado)
+	PentagonoRegular (int Lado)//Se hace la referencia a otra variable creada
 	{
-		this.Lado = Lado;
+		this.Lado = Lado;//Usando "this" se hace referencia q son dos variables lados diferentes
 	}
 	
-	int getLado ()
+	int getLado ()// se hace el desarrollo de lo que se pide con int
 	{
-		return Lado;
+		return Lado;//Devuelve el resultado de ladp
 	}
 	
-	void setLado(int Lado)
+	void setLado(int Lado)//Se hace la funcion setLado
 	{
-		this.Lado = Lado;
+		this.Lado = Lado;//Se hace hace referencia a el lado del set no al principal para diferenciarlos
 	}
 	
-	int getPerimetro ()
+	int getPerimetro ()//Se calcula el perimetro con la funcion getPerimetro
 	{
-		return Lado*5;
+		return Lado*5;//Devuelve el resultado del perimetro
 	}
 
 	public static void main(String[] args) {
