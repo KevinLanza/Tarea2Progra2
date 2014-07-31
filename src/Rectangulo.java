67ec40a4675dabea4fd3fbec78c7fe9675446576
KevinLanza <klanza03@gmail.com>
2014-07-31 07:41:50 -0600

    Update Rectangulo.java

--- src/Rectangulo.java
+++ src/Rectangulo.java
@@ -1,43 +1,43 @@
 
 public class Rectangulo {
 	
-	public int Base;
-	public int Altura;
+	public int Base;// Se hace la variable "Base" para el ejercicio
+	public int Altura;// Se hace la variable "Altura" para el ejercicio
 	
-	Rectangulo(int Base, int Altura)
+	Rectangulo(int Base, int Altura)//Se hace la formula del rectangulo
 	{
-		this.Base = Base;
-		this.Altura = Altura;
+		this.Base = Base;//Se usa el "this" para hacer referencia q son variables diferentes
+		this.Altura = Altura;//Se usa el "this" para hacer referencia q son variables diferentes
 	}
 	
-	int getBase ()
+	int getBase ()//Se hace la funcion getBase de tipo int
 	{
-		return Base;
+		return Base;//Devuelve el valor de la variable "Base"
 	}
 	
-	void setBase (int Base)
+	void setBase (int Base)//Se hace la funcion setBase de tipo void
 	{
-		this.Base = Base;
+		this.Base = Base;//Se usa "this" para diferenciar las variables 
 	}
 	
-	int getAltura ()
+	int getAltura ()//Se hace la funcion getAltura de tipo int
 	{
-		return Altura;
+		return Altura;//Devuelve el valor de la variable altura  
 	}
 	
-	void setAltura (int Altura)
+	void setAltura (int Altura)//Se hace la funcion setAltura de tipo void
 	{
-		this.Altura = Altura;
+		this.Altura = Altura;//Se usa "this" para diferenciar las variables 
 	}
 	
-	int getArea()
+	int getArea()//Se hace la funcion getArea de tipo int
 	{
-		return Base*Altura;
+		return Base*Altura;//Devuelve le valor de la funcion Area multiplicando "Base" * "Altura"
 	}
 	
-	int getPerimetro()
+	int getPerimetro()//Se hace la funcion getPerimetro de tipo int
 	{
-		return ((Base*2)+(Altura*2));
+		return ((Base*2)+(Altura*2));//Devuelve el valor del perimetro con respecto a su formula 
 	}
 
 	public static void main(String[] args) {
